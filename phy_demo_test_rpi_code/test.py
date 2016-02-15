9e424ef7eba81a467d51d786bfae64d1aba73d87
griffegg <gggriffes@gmail.com>
2016-02-15 00:29:29 +0000

    ADC working now

--- phy_demo_test_rpi_code/test.py
+++ phy_demo_test_rpi_code/test.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 from sense_hat import SenseHat
 import RPi.GPIO as GPIO
-import time                 # for time delays
+import time, sys, os
 import pigpio # http://abyz.co.uk/rpi/pigpio/python.html
+# Add the directory containing your module to the Python path (wants absolute paths)
+scriptpath = "/home/pi/Projects/Adafruit-Raspberry-Pi-Python-Code/Adafruit_ADS1x15"
+sys.path.append(os.path.abspath(scriptpath))
+from Adafruit_ADS1x15 import ADS1x15
 
 # Constants
 PUMP_GPIO = 4     # GPIO pin
@@ -27,8 +31,31 @@ LEAK_LED_GPIO = 25     # GPIO pin
 BLOCKAGE_LED_GPIO = 8     # GPIO pin
 HEARTBEAT_LED_GPIO = 7     # GPIO pin
 
-#global flow_count
-#flow_count = 0
+# Initialize the Analog to Digital converter for reading the current sensor
+ADS1015 = 0x00  # 12-bit ADC
+ADS1115 = 0x01	# 16-bit ADC
+
+# Select the gain
+# gain = 6144  # +/- 6.144V
+gain = 4096  # +/- 4.096V
+# gain = 2048  # +/- 2.048V
+# gain = 1024  # +/- 1.024V
+# gain = 512   # +/- 0.512V
+# gain = 256   # +/- 0.256V
+
+# Select the sample rate
+# sps = 8    # 8 samples per second
+# sps = 16   # 16 samples per second
+# sps = 32   # 32 samples per second
+# sps = 64   # 64 samples per second
+# sps = 128  # 128 samples per second
+sps = 250  # 250 samples per second
+# sps = 475  # 475 samples per second
+# sps = 860  # 860 samples per second
+
+# Initialise the ADC using the default mode (use default I2C address)
+# Set this to ADS1015 or ADS1115 depending on the ADC you are using!
+DISTANCE_HANDLE = ADS1x15(ic=ADS1115)
 
 # Initialize
 pi = pigpio.pi()           # Connect to Pi.
@@ -83,13 +110,17 @@ try:
             GPIO.output(VALVE_GPIO, VALVE_ON)
 
             while run_button == 0:
-                run_button = GPIO.input(RUN_GPIO)
+# read A to D converter to measure valve current
+                valve_mv = DISTANCE_HANDLE.readADCSingleEnded(0, gain, sps)
+
 # count the number of pulses in one second
                 flow_count = 0
                 time.sleep(1)           # wait 
                 gpm_flow_count = flow_count
-                sense.show_message("{}".format(gpm_flow_count), text_colour=red)
-                print ("{}".format(gpm_flow_count))
+#                sense.show_message("{} GPM {}mv".format(gpm_flow_count, valve_mv), text_colour=red)
+                print ("{} GPM {}mv".format(gpm_flow_count, valve_mv))
+
+                run_button = GPIO.input(RUN_GPIO)
 
 # turn off the pump and valve
             GPIO.output(PUMP_GPIO, PUMP_OFF)
