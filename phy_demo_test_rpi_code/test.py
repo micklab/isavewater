9c1c110e3542b6ccf4bf5bd2f4302141551d5fbb
griffegg <gggriffes@gmail.com>
2016-02-19 23:31:31 +0000

    modified for different GPIOs

--- phy_demo_test_rpi_code/test.py
+++ phy_demo_test_rpi_code/test.py
@@ -3,6 +3,8 @@ from sense_hat import SenseHat
 import RPi.GPIO as GPIO
 import time, sys, os
 import pigpio # http://abyz.co.uk/rpi/pigpio/python.html
+from webcolors import name_to_rgb
+
 # Add the directory containing your module to the Python path (wants absolute paths)
 scriptpath = "/home/pi/Projects/Adafruit-Raspberry-Pi-Python-Code/Adafruit_ADS1x15"
 sys.path.append(os.path.abspath(scriptpath))
@@ -14,11 +16,11 @@ PUMP_GPIO = 17     # GPIO pin
 PUMP_OFF = 1
 PUMP_ON = 0
 
-VALVE_GPIO = 4     # GPIO pin
+VALVE_GPIO = 26     # GPIO pin
 VALVE_OFF = 1
 VALVE_ON = 0
 
-FLOW_GPIO = 22     # GPIO pin
+FLOW_GPIO = 6     # GPIO pin
 
 RUN_GPIO = 27     # GPIO pin
 RUN_OFF = 1
@@ -29,6 +31,7 @@ LED_ON = 0
 BAD_VALVE_LED_GPIO = 18     # GPIO pin
 BLOCKAGE_LED_GPIO = 15     # GPIO pin
 LEAK_LED_GPIO = 14     # GPIO pin
+OVERCURRENT_GPIO = 16     # GPIO pin
 
 # Initialize the Analog to Digital converter for reading the current sensor
 ADS1015 = 0x00  # 12-bit ADC
@@ -69,6 +72,7 @@ GPIO.setup(VALVE_GPIO, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(FLOW_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(RUN_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(OVERCURRENT_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(BAD_VALVE_LED_GPIO, GPIO.OUT)
 GPIO.setup(LEAK_LED_GPIO, GPIO.OUT)
@@ -91,6 +95,10 @@ def flow_callback(gpio, level, tick):
 
 cb = pi.callback(FLOW_GPIO, pigpio.FALLING_EDGE, flow_callback) # interrupt mechanism
 
+LPM2GPM = 0.264172  # Liters per minute to gallons per minute factor
+def flow_calculator (freq):
+    return (LPM2GPM*(freq/5.5))
+    
 try:
     print("Turn on the RUN button when ready\n>")        
     while True:
@@ -102,8 +110,22 @@ try:
 # turn on the pump and valve
             GPIO.output(PUMP_GPIO, PUMP_ON)
             GPIO.output(VALVE_GPIO, VALVE_ON)
+            time.sleep(0.3)           # wait 
 
-            while run_button == 0:
+# check for Over Current condition on valve
+            overcurrent = 1
+            overcurrent = GPIO.input(OVERCURRENT_GPIO)
+            if (overcurrent == 0):
+                GPIO.output(PUMP_GPIO, PUMP_OFF)
+                GPIO.output(VALVE_GPIO, VALVE_OFF)
+                sense.show_message("OC!", text_colour=name_to_rgb("red"), back_colour=name_to_rgb("black"))
+                print ("OVERCURRENT WARNING! Zone 1 Valve failure!")
+                time.sleep(0.3)
+                sense.clear
+                time.sleep(0.3)
+ 
+            while (run_button == 0 and overcurrent == 1):
+
 # read A to D converter to measure valve current
                 valve_mv = 0.0
                 valve_mv = DISTANCE_HANDLE.readADCSingleEnded(0, gain, sps)
@@ -111,22 +133,26 @@ try:
 # count the number of pulses in one second
                 flow_count = 0
                 time.sleep(1)           # wait 
-                gpm_flow_count = flow_count
-#                sense.show_message("{} GPM {}mv".format(gpm_flow_count, valve_mv), text_colour=red)
-                print ("{} GPM {}mv".format(gpm_flow_count, valve_mv))
+                gpm_flow_count = int(flow_calculator(flow_count))
+                sense.show_message("{}".format(gpm_flow_count), text_colour=name_to_rgb("teal"))
+                print ("{} GPM {:4.1f} mv".format(gpm_flow_count, valve_mv))
 
+                run_button = 1
                 run_button = GPIO.input(RUN_GPIO)
+                overcurrent = 1
+                overcurrent = GPIO.input(OVERCURRENT_GPIO)
 
 # turn off the pump and valve
             GPIO.output(PUMP_GPIO, PUMP_OFF)
             GPIO.output(VALVE_GPIO, VALVE_OFF)
 
         else:
+            sense.clear
             time.sleep(1)           # wait
 
-            GPIO.output(BAD_VALVE_LED_GPIO, LED_ON)
-            GPIO.output(LEAK_LED_GPIO, LED_ON)
-            GPIO.output(BLOCKAGE_LED_GPIO, LED_ON)
+#            GPIO.output(BAD_VALVE_LED_GPIO, LED_ON)
+#            GPIO.output(LEAK_LED_GPIO, LED_ON)
+#            GPIO.output(BLOCKAGE_LED_GPIO, LED_ON)
             
         
 except KeyboardInterrupt:
