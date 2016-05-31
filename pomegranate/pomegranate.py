668e7548e79826184e18da8831b203463e228549
griffegg <gggriffes@gmail.com>
2016-05-31 11:07:16 +0000

    fixing ammeter

--- pomegranate/pomegranate.py
+++ pomegranate/pomegranate.py
@@ -6,18 +6,7 @@ https://www.hackster.io/peejster/rover-c42139
 import RPi.GPIO as GPIO
 import time, sys, os
 import pigpio # http://abyz.co.uk/rpi/pigpio/python.html
-
-# Add the directory containing your module to the Python path (wants absolute paths)
-#scriptpath = "/home/pi/projects/Adafruit-Raspberry-Pi-Python-Code/Adafruit_ADS1x15"
-#sys.path.append(os.path.abspath(scriptpath))
-#
-# Install using this:
-# sudo apt-get update
-# sudo apt-get install -y python3 python3-pip python-dev
-# sudo pip3 install rpi.gpio
-# sudo pip install adafruit-ads1x15
-# Import the ADS1x15 module.
-import Adafruit_ADS1x15
+import spidev
 
 global flow_count
 flow_count = 0
@@ -41,6 +30,8 @@ class flow_sensor(object):
 # Done with initialization
                     
     def __del__(self):
+        cb.cancel() # Cancel callback.
+        pi.stop()   # Disconnect from Pi.
         print 'flow '+str(self.flow_zone)+' closed'
 
     def flow_callback(self, gpio, level, tick):
@@ -69,38 +60,29 @@ class valve_current(object):
     def __init__(self, valve_zone):
 
         self.valve_zone = valve_zone
+        self.spi = spidev.SpiDev()
+        self.spi.open(0,0)
 
-        # Initialize the Analog to Digital converter for reading the current sensor
-        self.ADS1015 = 0x00  # 12-bit ADC
-        self.ADS1115 = 0x01	# 16-bit ADC
-
-        # Select the gain
-        # gain = 6144  # +/- 6.144V
-        self.gain = 4096  # +/- 4.096V
-        # gain = 2048  # +/- 2.048V
-        # gain = 1024  # +/- 1.024V
-        # gain = 512   # +/- 0.512V
-        # gain = 256   # +/- 0.256V
-
-        # Select the sample rate
-        # sps = 8    # 8 samples per second
-        # sps = 16   # 16 samples per second
-        # sps = 32   # 32 samples per second
-        # sps = 64   # 64 samples per second
-        # sps = 128  # 128 samples per second
-        self.sps = 250  # 250 samples per second
-        # sps = 475  # 475 samples per second
-        # sps = 860  # 860 samples per second
-
-        # Initialise the ADC using the default mode (use default I2C address)
-        # Set this to ADS1015 or ADS1115 depending on the ADC you are using!
-
-        #DISTANCE_HANDLE = ADS1x15(ic=ADS1115)
+    def calculate_milliamps(self, adc_value):
+        adc_value_norm = adc_value - 2048
+        return float((1000.0 * float(adc_value_norm)) / 89.95)
 
     def get_current(self):
         self.current = 0.0
 
-        print 'valve current = '+str(self.current)+' in amps'
+        while True:
+            response = self.spi.readbytes(2)
+            print response
+            lsb = response[1]
+            msb = response[0]
+            print hex(msb)
+            print hex(lsb)
+            pmod_value = (msb << 8) | lsb
+            print hex(pmod_value)
+            print self.calculate_milliamps(pmod_value)
+            time.sleep(1)
+
+#        print 'valve current = '+str(self.current)+' in amps'
         return self.current
 
 ##########################
@@ -163,8 +145,6 @@ if __name__ == '__main__':
             time.sleep(0.5)
             
     except KeyboardInterrupt:
-        cb.cancel() # Cancel callback.
-        pi.stop()   # Disconnect from Pi.
         exit()
 
 #    except:
