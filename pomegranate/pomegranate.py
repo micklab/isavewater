eaab84271049a0bd434f63744f8a9accca129f14
griffegg <gggriffes@gmail.com>
2016-06-03 18:17:23 -0700

    fixed multiple valve daemons

--- pomegranate/pomegranate.py
+++ pomegranate/pomegranate.py
@@ -24,15 +24,13 @@ logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s
 # -----------------------------------
 # setup the daemon that measures valve current
 # -----------------------------------
-# this is a queue that keeps a list of the most recent measurements
-currentq = deque(maxlen = 200)
-
 # this daemon is a background process that runs continuously makes current measurements
 # and fills the queue with current measurements. 
-def daemon_valve_1():
-    logging.debug('Starting daemon_valve_1')
+def daemon_valve(valve_number, spi_addr):
+    logging.debug('daemon valve # '+str(valve_number)+' running: spi='+str(spi_addr))
     spi = spidev.SpiDev()
-    spi.open(0,0)
+    spi.open(0,spi_addr)
+    x = 1
     while True:
         response = spi.readbytes(2)
         lsb = response[1]
@@ -40,16 +38,13 @@ def daemon_valve_1():
         pmod_value = (msb << 8) | lsb
         adc_value_norm = pmod_value - 2048
         ma = float((1000.0 * float(adc_value_norm)) / 89.95)
-#        logging.debug('Current(daemon) = {:5.1f}'.format(ma) + ' ma')
-        currentq.append(ma)
-# this has to happen fairly quickly because the measurements vary wildly.
-# the most important measurement is the maximum positive value which corresponds
-# to the actual current measurement and appears about once every 50 - 100 measurements
+#        logging.debug('valve['+str(valve_number)+'] = {:5.1f}'.format(ma) + ' ma')
+        current_queue[valve_number].append(ma)
+    # this has to happen fairly quickly because the measurements vary wildly.
+    # the most important measurement is the maximum positive value which corresponds
+    # to the actual current measurement and appears about once every 50 - 100 measurements
         time.sleep(0.1)
-
-# create the thread and make it a daemon (doesn't block the main program calling it)
-measure_valve_current = threading.Thread(name='daemon_valve_1', target=daemon_valve_1)
-measure_valve_current.setDaemon(True)
+#        logging.debug(current_queue[valve_number])
 
 # -----------------------------------
 # setup the daemon that measures flow
@@ -130,10 +125,9 @@ class flow_sensor(object):
 class valve_current(object):
 ############################
 
-    def __init__(self, valve_zone, current_list):
+    def __init__(self, valve_zone):
 
         self.valve_zone = valve_zone
-        self.current = current_list
         
     def __del__(self):
         print 'valve current '+str(self.valve_zone)+' closed'
@@ -144,14 +138,14 @@ class valve_current(object):
     def get_current(self):
 
 # find the maximum value in the set of measurements
-        max_value=0
-        for i,value in enumerate(self.current):
+        max_value=-6000.0
+        for i,value in enumerate(current_queue[self.valve_zone]):
             if value>max_value:
                 max_value=value
                 index=i
 #        max_value = max(self.current_list) # this doesn't work returning the error "float object is not callable"
 
-#        print max_value
+#        print current_queue[self.valve_zone]
 
         return max_value
 
@@ -163,10 +157,10 @@ class valve_power(object):
         self.valve_gpio = valve_gpio
         self.valve_zone = valve_zone
 
+        print 'valve '+str(valve_zone)+' status GPIO # =  '+str(self.valve_gpio)
+        
         GPIO.setup(self.valve_gpio, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
       
-#        print 'valve status GPIO # =  '+str(self.valve_gpio)
-        
 # Done with initialization
                     
     def __del__(self):
@@ -187,57 +181,74 @@ if __name__ == '__main__':
     VALVE_POWER_GPIOS = [27, 17]     # GPIO pins assigned to valves
     VALVE_CURRENT_SPI_ADDR = [0, 1]  # SPI addresses assigned to valve current meters
 
-    valves = []
-    valve_power_object = []
-    valve_current_object = []
+    VALVE_CURRENT_QUEUE_SIZE = 200
 
     VALVE_NUMBER = 0
     VALVE_STATUS = 1
     VALVE_GPIO = 2
     VALVE_SPI = 3
-    for v in range(NUMBER_OF_VALVES):
-        valves.append([v, VALVE_OFF, VALVE_POWER_GPIOS[v], VALVE_CURRENT_SPI_ADDR[v]])   # valve number, valve status, GPIO pin for valve status, SPI address for valve current
     
-    logging.debug('valves[] = '+str(valves))
-        
     try:
         
         flow_sensor_1 = flow_sensor(1, flowq)
 
+        # create the data structures
+        valves = []
+        current_queue = []
         for v in range(NUMBER_OF_VALVES):
-            valve_power_object.append(valve_power(v, VALVE_POWER_GPIOS[v]))
+            valves.append([v, VALVE_OFF, VALVE_POWER_GPIOS[v], VALVE_CURRENT_SPI_ADDR[v]])   # valve number, valve status, GPIO pin for valve status, SPI address for valve current
+            # this is a queue that keeps a list of the most recent measurements
+            current_queue.append(deque(maxlen = VALVE_CURRENT_QUEUE_SIZE))
 
-        valve_current_1 = valve_current(1, currentq)
+        logging.debug('valves[] = '+str(valves))
+        
+        # create the objects
+        valve_power_object = []
+        valve_current_object = []
+        for v in range(NUMBER_OF_VALVES):
+            valve_power_object.append(valve_power(v, valves[v][VALVE_GPIO]))
+            valve_current_object.append(valve_current(v))
+
+        # create the current measurement daemons
+        for v in range(NUMBER_OF_VALVES):
+            # create the thread and make it a daemon (doesn't block the main program calling it)
+#            logging.debug('creating daemon for valve '+str(v))
+            valve_current_daemon = threading.Thread(name='daemon_valve', target=daemon_valve, args=(v, valves[v][VALVE_SPI]))
+#            logging.debug('setting daemon for valve '+str(v))
+            valve_current_daemon.setDaemon(True)
+#            logging.debug('starting daemon for valve '+str(v))
+            valve_current_daemon.start()
+        
+
+        loop = 0    # used to determine the first time through each valve_status change
         current = 0.0
+
+        # start the sensor daemons
+        measure_flow_1.start()
+        # start the valve daemons
 
         #############
         #### Start of Main Loop
         #############
 
-        loop = 0    # used to determine the first time through each valve_status change
-
-        # start the sensor daemons
-        measure_flow_1.start()
-        measure_valve_current.start()
-        
         while True:
             time.sleep(10)                      # let the queues fill with values
 
             for v in range(NUMBER_OF_VALVES):
                 valve_status = valve_power_object[v].get_status()
                 if (valve_status == 1 and loop == 0):    #first time valve turns on
-                    valve_current_1.clear_queue
+                    valve_current_object[v].clear_queue
                     flow_sensor_1.clear_queue
                     loop = 1
                 elif (valve_status == 0 and loop == 1):    #first time valve turns off
-                    valve_current_1.clear_queue
+                    valve_current_object[v].clear_queue
                     flow_sensor_1.clear_queue
                     loop = 0
 
                 flow = flow_sensor_1.get_rate()
-                current = valve_current_1.get_current()
+                current = valve_current_object[v].get_current()
 
-                print 'V['+str(v)+','+str(valve_status)+']:F[{:03.4f}'.format(flow)+']:C[{:05.1f}'.format(current)+']:T['+str(datetime.datetime.now())+']'
+                print 'V['+str(v)+','+str(valve_status)+']:F[{:4.4f}'.format(flow)+']:C[{:7.1f}'.format(current)+']:T['+str(datetime.datetime.now())+']'
    
 ###########################################################
 # END
